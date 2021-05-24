6d7f9c4904f69f3a266e4d03ed0d8faca23d6118
Jacob Hughes <jacob.hughes@myunidays.com>
2021-05-24 13:52:34 +0100

    Add HeaderTestModule package

--- Package.swift
+++ Package.swift
@@ -11,12 +11,19 @@ let package = Package(
     products: [
         .library(name: "KMPExampleModule",
                  targets: ["KMPExampleModule"]),
+        .library(name: "HeaderTestModule",
+                 targets: ["HeaderTestModule"]),
         ],
     targets: [
         .binaryTarget(
            name: "KMPExampleModule",
            url: "https://rome-ios-unidays-dev.s3-eu-west-1.amazonaws.com/test-dependencies/KMPExampleModule-1.0.0.zip",
            checksum: "4923284886ad04361bc37cb5137c0bb437cdf99c0aebe1505733665f8cd8c524"
+        ),
+        .binaryTarget(
+           name: "HeaderTestModule",
+           url:"https://rome-ios-unidays-dev.s3-eu-west-1.amazonaws.com/test-dependencies/HeaderTestModule-1.0.0.zip",
+           checksum: "1f268283b20ed591daafbc772be8a482d05ffb0055c10068c8b7553ebfa20321"
         )
     ]
 )
